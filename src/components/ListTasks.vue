7cd527c2b11c1df826400880ed66f0373c726006
shaolinmkz <nwabuzor.obiora@gmail.com>
2022-01-04 23:49:42 +0100

    abstract todo list component

---
+++ src/components/ListTasks.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-list subheader two-line flat>
+    <div
+      v-for="task in $store.state.tasks"
+      :key="task.id"
+      :class="{ 'blue lighten-5': task.completed }"
+    >
+      <v-list-item @click="doneTask(task.id)">
+        <template v-slot:default>
+          <v-list-item-action>
+            <v-checkbox
+              :input-value="task.completed"
+              color="primary"
+            ></v-checkbox>
+          </v-list-item-action>
+
+          <v-list-item-content>
+            <v-list-item-title
+              :class="{ 'text-decoration-line-through': task.completed }"
+              >{{ task.title }}</v-list-item-title
+            >
+          </v-list-item-content>
+
+          <v-list-item-action>
+            <v-btn icon @click.stop="deleteTask(task.id)">
+              <v-icon color="primary lighten-1">mdi-delete</v-icon>
+            </v-btn>
+          </v-list-item-action>
+        </template>
+      </v-list-item>
+      <v-divider></v-divider>
+    </div>
+  </v-list>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  methods: {
+    doneTask(id: number) {
+      this.$store.commit("doneTask", id);
+    },
+    deleteTask(id: number) {
+      this.$store.commit("deleteTask", id);
+    },
+  },
+});
+</script>
+
+<style>
+</style>
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -3,44 +3,13 @@
     <div class="mb-3">
       <field-add-task />
     </div>
-    <v-list subheader two-line flat v-if="$store.state.tasks.length">
-      <div
-        v-for="task in $store.state.tasks"
-        :key="task.id"
-        :class="{ 'blue lighten-5': task.completed }"
-      >
-        <v-list-item @click="doneTask(task.id)">
-          <template v-slot:default>
-            <v-list-item-action>
-              <v-checkbox
-                :input-value="task.completed"
-                color="primary"
-              ></v-checkbox>
-            </v-list-item-action>
 
-            <v-list-item-content>
-              <v-list-item-title
-                :class="{ 'text-decoration-line-through': task.completed }"
-                >{{ task.title }}</v-list-item-title
-              >
-            </v-list-item-content>
-
-            <v-list-item-action>
-              <v-btn icon @click.stop="deleteTask(task.id)">
-                <v-icon color="primary lighten-1">mdi-delete</v-icon>
-              </v-btn>
-            </v-list-item-action>
-          </template>
-        </v-list-item>
-        <v-divider></v-divider>
-      </div>
-    </v-list>
+    <list-tasks v-if="!!$store.state.tasks.length" />
 
     <v-alert
       v-else
       type="info"
       icon="mdi-check-all"
-      value="true"
       >No Task</v-alert
     >
   </div>
@@ -49,20 +18,14 @@
 <script lang="ts">
 import Vue from "vue";
 import FieldAddTask from "../components/Todo/FieldAddTask.vue";
+import ListTasks from "@/components/ListTasks.vue";
 
 export default Vue.extend({
   name: "Home",
-
-  methods: {
-    doneTask(id: number) {
-      this.$store.commit("doneTask", id);
-    },
-    deleteTask(id: number) {
-      this.$store.commit("deleteTask", id);
-    },
-  },
   components: {
     'field-add-task' : FieldAddTask,
+    'list-tasks' : ListTasks,
+    ListTasks,
   },
 });
 </script>
